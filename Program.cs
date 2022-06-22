d892985010fed7edbfa93c1c782ff9547154da3a
SynergyUSAllc <94911727+SynergyUSAllc@users.noreply.github.com>
2022-06-22 19:19:09 +0300

    SynergyAutoLogOff

---
+++ Program.cs
@@ -0,0 +1,186 @@
+﻿using System;
+
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+using System.Threading;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace AutoLogoutConsole
+{
+    public static class InputTimer
+    {
+        //
+        //
+        internal struct LASTINPUTINFO
+        {
+            public uint cbSize;
+
+            public uint dwTime;
+        }
+
+        //
+        private static BackgroundWorker worker = new BackgroundWorker();
+
+        //
+        //
+        [DllImport("User32.dll")]
+        private static extern bool GetLastInputInfo(ref LASTINPUTINFO plii);
+
+        [DllImport("Kernel32.dll")]
+        private static extern uint GetLastError();
+
+        public static uint GetIdleTime()
+        {
+            LASTINPUTINFO lastInPut = new LASTINPUTINFO();
+            lastInPut.cbSize = (uint)System.Runtime.InteropServices.Marshal.SizeOf(lastInPut);
+            GetLastInputInfo(ref lastInPut);
+
+            return ((uint)Environment.TickCount - lastInPut.dwTime);
+        }
+
+        public static long GetTickCount()
+        {
+            return Environment.TickCount;
+        }
+
+        public static long GetLastInputTime()
+        {
+            LASTINPUTINFO lastInPut = new LASTINPUTINFO();
+            lastInPut.cbSize = (uint)System.Runtime.InteropServices.Marshal.SizeOf(lastInPut);
+            if (!GetLastInputInfo(ref lastInPut))
+            {
+                throw new Exception(GetLastError().ToString());
+            }
+
+            return lastInPut.dwTime;
+        }
+
+        //
+        //
+
+        //
+
+        //
+        //
+        //CODE FOR LOG OFF
+        //
+        //
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool ExitWindowsEx(uint uFlags, uint dwReason);
+
+        public static bool WindowsLogOff()
+        {
+            // Console.WriteLine("debugger");
+            return ExitWindowsEx(0 | 0x00000004, 0);
+        }
+
+        //
+        //
+        //
+
+        //
+        //
+        //WORKER CODE
+        //
+        //
+        //
+        private static void worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            // Console.WriteLine(e.ProgressPercentage.ToString());
+        }
+
+        private static void worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            //   Console.WriteLine("Starting to do some work now...");
+            //  double idlelogout = 10;
+            //    Console.WriteLine("*****IDLE MONITOR*****");
+
+            uint wrkGetIdleTime;
+
+            while (true)
+            {
+                wrkGetIdleTime = GetIdleTime() / 1000;
+
+                //  Console.WriteLine(wrkGetIdleTime);
+                Thread.Sleep(200);
+
+                int getTimeFromXmlInSeconds = GetTimeFromXML();
+
+                if (wrkGetIdleTime > getTimeFromXmlInSeconds) WindowsLogOff();
+                // if (wrkGetIdleTime > 20) WindowsLogOff();                //Log off after 20 seconds
+
+                //if (InputTimer.GetInputIdleTime().Seconds <= idlelogout)
+                //{
+                //    Console.WriteLine("Nothing, nevermind, IDLE time: " + InputTimer.GetInputIdleTime().Seconds + " seconds");
+                //}
+                //else
+                //{
+                //    Console.WriteLine("Logout, IDLE time: " + InputTimer.GetInputIdleTime().Seconds + " seconds");
+                //    WindowsLogOff();
+                //}
+            }
+        }
+
+        private static void worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            //  Console.WriteLine("Done...");
+            //   Console.WriteLine("All inactive users are logged off");
+        }
+
+        public static int GetTimeFromXML()
+        {
+            try
+            {
+                String RunPath = AppDomain.CurrentDomain.BaseDirectory;
+
+                XmlDocument doc = new XmlDocument();
+                doc.Load(RunPath + "Time.xml");
+
+                XmlElement root = doc.DocumentElement;
+                XmlNodeList nodes = root.SelectNodes("LogOffTime");
+
+                int minutes = 15; //set a default time just in case
+                int seconds = 20 * 60;
+
+                foreach (XmlNode node in nodes)
+                {
+                    foreach (XmlNode childNode in node.ChildNodes)
+                    {
+                        if (childNode.Name == "minutes")
+                        {
+                            minutes = Convert.ToInt32(childNode.InnerText);
+                            seconds = minutes * 60;
+                            return seconds;
+                        }
+                    }
+                }
+                return seconds;
+            }
+            catch { return 20 * 60; }
+        }
+
+        public static void Main()
+        {
+            //  int x = GetTimeFromXML();
+            worker.DoWork += worker_DoWork;
+            worker.RunWorkerCompleted += worker_RunWorkerCompleted;
+            worker.ProgressChanged += worker_ProgressChanged;
+
+            worker.WorkerReportsProgress = true;
+            worker.WorkerSupportsCancellation = true;
+
+            // Console.WriteLine("Starting Application...");
+
+            worker.RunWorkerAsync();
+            while (true)
+            {
+                Thread.Sleep(1000);
+            }
+            //  Console.ReadKey();
+        }
+    }
+}
---
+++ SynergyAutoLogOffInstallForAllUsers/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SynergyAutoLogOffInstallForAllUsers
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+                String RunPath = AppDomain.CurrentDomain.BaseDirectory;
+
+                string ProgramPath = "\"" + RunPath + "SynergyAutoLogOff.exe" + "\"";
+                Registry.SetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run", "SynergyAutoLogOff", ProgramPath);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey();
+                return;
+            }
+
+            Console.WriteLine("Success. Please Log Off / Log On to start the application.");
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
